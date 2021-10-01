55f0fbd10637f890ba7332b78f6c8583d9c417a0
Arecio Canton <cuba289@gmail.com>
2021-09-30 20:14:19 -0400

    changed logo

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link as LR } from 'react-router-dom';
-// import { Link as LS } from 'react-scroll';
+import { Link as LS } from 'react-scroll';
 
 export const Nav = styled.nav`
     height: 80px;
@@ -42,9 +42,50 @@ export const NavLogo = styled(LR)`
     letter-spacing: .5px;
 `
 
-export const Bird = styled.img`
-    background-color: #FAF1CF;
-    border-radius: 50%;
-    padding: 5px;
+// export const Bird = styled.img`
+//     background-color: #FAF1CF;
+//     border-radius: 50%;
+//     padding: 5px;
+// `
+export const MobileIcon = styled.div`
+    display: none;
 
+    @media screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 0;
+        transition: translate(-100%, 60%);
+        font-size: 1.8rem;
+        cursor: pointer;
+    }
+`
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
+
+export const NavItem = styled.li`
+    height: 80px;
+`
+
+export const NavLinks = styled(LS)`
+    color: #FFF;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1 rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active {
+        border-bottom: 3px solid #01bf71;
+    }
 `
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { Nav, NavbarContainer, NavLogo, Bird } from './NavbarElements';
-import logo from '../../images/hummingbird.png';
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import { GiHummingbird } from "react-icons/gi";
+import { IconContext } from "react-icons";
 
 const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">Hummingbird<br/> Lawn Services LLC</NavLogo>
-                    <Bird src={logo} alt="hummingbird" />
-                </NavbarContainer>
-            </Nav>
-        </>
-    )
-}
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <IconContext.Provider
+              value={{ color: "#FEFEFE", style: {marginRight: '20px'} }}>
+              <div>
+                <GiHummingbird/>
+              </div>
+            </IconContext.Provider>
+            Hummingbird
+            <br /> Lawn Services LLC
+          </NavLogo>
+          {/* <Bird src={logo} alt="hummingbird" /> */}
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
