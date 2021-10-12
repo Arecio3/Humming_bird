91e7f96f1ae2a1b0bb7b0d8f8e6b129606526344
Arecio Canton <cuba289@gmail.com>
2021-10-12 17:13:50 -0400

    Added info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+import car from '../../images/svg-2.svg';
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'HummingBird',
+    headLine: 'Premium Lawn Services',
+    description: "Contact me today through our company website for a free estimate! Se habla Español",
+    buttonLabel: 'Free Estimate',
+    imgStart: false,
+    img: car,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
