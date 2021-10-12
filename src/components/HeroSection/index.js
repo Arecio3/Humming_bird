aa4648fa3afe8b703b4494d2ebe8c690ce3c8542
Arecio Canton <cuba289@gmail.com>
2021-10-12 15:07:56 -0400

    Added hero section title and button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { HeroContainer, HeroBg, VideoBg, MobileImg } from "./HeroElements";
+import React, { useState } from "react";
+import { HeroContainer, HeroBg, VideoBg, MobileImg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from "./HeroElements";
+import { Button } from '../ButtonElements';
 import Video from "../../videos/birdvideo.mp4";
 import Bird from '../../images/bird.jpg'
+
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
   return (
     <HeroContainer>
       <HeroBg>
@@ -22,7 +30,7 @@ const HeroSection = () => {
             Contact me for a <b>FREE</b> estimate. No need for an account!
           </HeroP>
           <HeroBtnWrapper>
-              <Button to='contact'>Get Started {hover ? <ArrowForward /> : <ArrowRight />}</Button>
+              <Button onMouseEnter={onHover} onMouseLeave={onHover} to='contact'>Get Started {hover ? <ArrowForward /> : <ArrowRight />}</Button>
           </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
