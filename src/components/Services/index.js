e821750e934c14223e419d41b816b682f741af86
Arecio Canton <cuba289@gmail.com>
2021-10-19 19:58:53 -0400

    translated services

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -2,26 +2,31 @@ import React from 'react'
 import Icon1 from '../../images/svg-1.svg'
 import Icon2 from '../../images/svg-2.svg'
 import Icon3 from '../../images/svg-3.svg'
+import { useTranslation } from 'react-i18next';
+
 import { ServicesContainer, ServicesH1, ServicesWrapper, ServicesIcon, ServicesCard, ServicesH2, ServicesP, } from './ServicesElements'
 const Services = () => {
+
+    const { t } = useTranslation();
+
     return (
         <ServicesContainer id="services">
-            <ServicesH1>Our Services</ServicesH1>
+            <ServicesH1>{t('ServicesH1')}</ServicesH1>
             <ServicesWrapper>
                 <ServicesCard>
                     <ServicesIcon src={Icon1} />
-                    <ServicesH2>Flexible</ServicesH2>
-                    <ServicesP>We make sure to give the best of the best service.</ServicesP>
+                    <ServicesH2>{t('Flexible')}</ServicesH2>
+                    <ServicesP>{t('FlexP')}</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon2} />
-                    <ServicesH2>Bi-Lingual</ServicesH2>
-                    <ServicesP>We make sure to give the best of the best service.</ServicesP>
+                    <ServicesH2>{t('BiLingual')}</ServicesH2>
+                    <ServicesP>{t('BiLingualP')}</ServicesP>
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon3} />
-                    <ServicesH2>Mobile</ServicesH2>
-                    <ServicesP>We make sure to give the best of the best service.</ServicesP>
+                    <ServicesH2>{t('Mobile')}</ServicesH2>
+                    <ServicesP>{t('MobileP')}</ServicesP>
                 </ServicesCard>
             </ServicesWrapper>
         </ServicesContainer>
