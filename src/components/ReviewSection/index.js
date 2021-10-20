f88c1ebecf87f1242a99ae1912034b4db400c1ef
Arecio Canton <cuba289@gmail.com>
2021-10-19 20:06:21 -0400

    translated services

--- src/components/ReviewSection/index.js
+++ src/components/ReviewSection/index.js
@@ -13,56 +13,45 @@ function Review() {
         <h5>{t("CustomersThink")}</h5>
       </div>
       <Carousel id="carousel" variant="dark">
-  <Carousel.Item>
-    <img
-      className="d-block w-100 review-img"
-      src={grass}
-      alt="First slide"
-    />
-    <Carousel.Caption className="caption">
-      <h3>{t('Quote1')}</h3>
-      <p id="name">Jelisa Hernandez</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 review-img"
-      src={grass}
-      alt="Second slide"
-    />
+        <Carousel.Item>
+          <img
+            className="d-block w-100 review-img"
+            src={grass}
+            alt="First slide"
+          />
+          <Carousel.Caption className="caption">
+            <h3>{t("Quote1")}</h3>
+            <p id="name">Jelisa Hernandez</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 review-img"
+            src={grass}
+            alt="Second slide"
+          />
 
-    <Carousel.Caption className="caption">
-      <h3>{t('Quote2')}</h3>
-      <p id="name">Jason Rohena</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 review-img"
-      src={grass}
-      alt="Third slide"
-    />
+          <Carousel.Caption className="caption">
+            <h3>{t("Quote2")}</h3>
+            <p id="name">Jason Rohena</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 review-img"
+            src={grass}
+            alt="Third slide"
+          />
 
-    <Carousel.Caption className="caption">
-      <h3>{t('Quote3')}</h3>
-      <p id="name">Jean Hernandez</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+          <Carousel.Caption className="caption">
+            <h3>{t("Quote3")}</h3>
+            <p id="name">Jean Hernandez</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
 
 export default Review;
 
-// <Carousel id="carousel" variant="dark">
-//   {data.map((d) => (
-//     <Carousel.Item interval={8000}>
-//       <img className="d-block w-100 review-img" src={d.img} alt={d.id} />
-//       <Carousel.Caption className="caption">
-//         <h3>{d.quote}</h3>
-//         <p id="name">{d.name}</p>
-//       </Carousel.Caption>
-//     </Carousel.Item>
-//   ))}
-// </Carousel>
